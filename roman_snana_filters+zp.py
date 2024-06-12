70d4fe8a3164627051ded479fbcecc33b11d3c13
Richard Kessler <kessler@kicp.uchicago.edu>
2024-06-12 15:00:41 -0500

    fix bug and read positional arg to get name of effective area file

--- roman_snana_filters+zp.py
+++ roman_snana_filters+zp.py
@@ -2,36 +2,36 @@
 #
 # Created Oct 4 2023 by R.Kessler
 #
-# Read Roman filter transmissions from multi-column file
-# (downloaded from
-#   https://github.com/GalSim-developers/GalSim/blob/releases/2.5/share/roman/Roman_effarea_20210614.txt
+# Read Roman filter transmissions from multi-column file with wave and filters.
 # + compute zero point avg/min/max/range. 
 # + Optionally write filter-transmission files for SNANA. 
 #
 # Note that wavelength units are microns and are converted here to Angstroms. 
 # Original use was for the Roman-DESC Image-sim project.
 #
-# May 2024: 
-#   + update with zeropoint calculation based on effective area and AB spectrum.
-# 
-# Jun 10 2024
-#   + specify effective area file as wildcard and loop over all 18 SCAs;
-#     Report average/min/max/stddev.
+# Example usage:
+#   roman_snana_filters+zp.py Roman_effarea_20210614.txt
+#     (process just one file)
+#
+#   roman_snana_filters+zp.py  Roman_effarea_v8_SCA\*_20240301.ecsv
+#         or
+#   roman_snana_filters+zp.py "Roman_effarea_v8_SCA*_20240301.ecsv"
+#      (process all 18 SCAs; note to use either double quotes with *, or \*)
 #
 # ===================
 
-import os, sys, glob
+import os, sys, glob, argparse
 import numpy as np
 
-#TRANS_INPUT   =  "Roman_effarea_20210614.txt"
-TRANS_INPUT   =  "Roman_effarea_tables_20240327/Roman_effarea_v8_SCA*_20240301.ecsv"
-
+# ===========================================================
+# these output keys need to be input command lines, but first need to
+# figure out what to write out when there are multiple filter files (e.g., per SCA)
 PREFIX_OUTPUT = "ROMAN"
-
-AREA_FUDGE  = 1.0E4  # /3.757   # from M.Jarvis slack
-
 WRITE_SNANA_FILTER_TRANS = False
 
+
+# if input filter name starts with generic F,
+# replace with more human readable filter band name.
 BAND_MAP = {
     'F062' : 'R062' ,
     'F087' : 'Z087' ,
@@ -46,6 +46,25 @@ BAND_MAP = {
 # ==================================================
 # ==================================================
     
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    # positional arg
+    msg = "name of effective area file (use wildcard * to average over many)"
+    parser.add_argument("input_file", help=msg,
+                        nargs="?", default=None)
+
+    #msg = "Create bash script but do not submit it "
+    #parser.add_argument("--nosubmit", "-n", help=msg,
+    #                        action="store_true")
+
+    # parse it
+    args = parser.parse_args()
+
+
+    return args
+    # end get_args
+
 def open_filter_trans_files(line0):
 
     # split first line of Roman-trans file and read list of filters,
@@ -79,6 +98,8 @@ def open_filter_trans_files(line0):
 
 def compute_zp(filt,tr_dict):
 
+    AREA_FUDGE  = 1.0E4  # m^2 -> cm^2 conversion
+
     LAM_ARRAY   = np.array(tr_dict['LAM_ARRAY'])  
     TRANS_ARRAY = np.array(tr_dict['TRANS_ARRAY'])
     TRANS_ARRAY = TRANS_ARRAY * AREA_FUDGE
@@ -94,7 +115,6 @@ def compute_zp(filt,tr_dict):
     mag     = 2.5*np.log10(AB_sum)
 
     zp = mag
-    #print(f"\t ZP_AB({filt}) = {zp:.3f}")
     return zp
     
 
@@ -152,30 +172,33 @@ def process_effarea(effarea_file):
 # ===================================
 if __name__ == "__main__":
 
-    effarea_file_list = glob.glob(TRANS_INPUT)
+    args = get_args()
+    effarea_file_list = glob.glob(args.input_file)
 
-    zp_sca_dict = {}
+    zp_dict = {}
 
     for effarea_file in effarea_file_list:
         filt_list, zp_list = process_effarea(effarea_file)
         
         # convert zp_list over filters into zp list over sca
         for filt, zp in zip(filt_list, zp_list):
-            if filt in zp_sca_dict:
-                zp_sca_dict[filt].append(zp)
-            else:
-                zp_sca_dict[filt] = []
+            if filt not in zp_dict:  zp_dict[filt] = []
+            zp_dict[filt].append(zp)
     
     # print and compute stats for each band
     n_effarea = len(effarea_file_list)
     print(f"\n  # Average ZP among {n_effarea} effective area files:")
     print(f"\n  # BAND   <ZP>   ZP_MIN-ZP_MAX   ZP_RANGE")
-    for filt, zp_sca_list in zp_sca_dict.items():
-        zp_avg = np.mean(zp_sca_list) 
-        zp_min = np.min(zp_sca_list) 
-        zp_max = np.max(zp_sca_list) 
+    for filt, zp_list in zp_dict.items():
+        zp_avg = np.mean(zp_list) 
+        zp_min = np.min(zp_list) 
+        zp_max = np.max(zp_list) 
         zp_range = zp_max - zp_min
-        band  = BAND_MAP[filt]
+        if filt in BAND_MAP:
+            band = BAND_MAP[filt]
+        else:
+            band = filt
+        
         print(f"    {band}  {zp_avg:.3f}  {zp_min:.3f}-{zp_max:.3f}     {zp_range:.2f}")
 
     # == END ===
